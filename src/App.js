e2074b8b360d720f0cad2278dcbbfb8e8b04bac8
rico260104 <rico260104@gmail.com>
2021-05-30 17:08:49 +0700

    CRUD-ReactJS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Crud from "./crud";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <Crud />
+    </div>
+  );
+}
+
+export default App;
